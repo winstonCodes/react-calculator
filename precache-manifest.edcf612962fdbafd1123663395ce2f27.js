8d4b30571e5c4ef0f994e3e84abeaff732cfc513
Winston Smith <winstonsmithcodes@gmail.com>
2019-01-25 18:19:53 +0700

    Updates

---
+++ precache-manifest.edcf612962fdbafd1123663395ce2f27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2ecf2e68de309a6bd2b",
+    "url": "/react-calculator/static/js/runtime~main.a2ecf2e6.js"
+  },
+  {
+    "revision": "3bf3846438178985237d",
+    "url": "/react-calculator/static/js/main.3bf38464.chunk.js"
+  },
+  {
+    "revision": "1d714d2257181748a59b",
+    "url": "/react-calculator/static/js/1.1d714d22.chunk.js"
+  },
+  {
+    "revision": "3bf3846438178985237d",
+    "url": "/react-calculator/static/css/main.ce6b4acb.chunk.css"
+  },
+  {
+    "revision": "d1d8ff75339c8c1169961f7b889a64f4",
+    "url": "/react-calculator/index.html"
+  }
+];
